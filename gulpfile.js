5b0d070132418972ea303571bb5bd8f92e99daba
ChristiaanCruzj <christiaancruzj@gmail.com>
2023-07-10 14:13:46 -0600

    convert images since gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-const { src, dest, watch } = require("gulp"); // i need this functions 
+const { src, dest, watch, parallel } = require("gulp"); // i need this functions 
+//css
 const sass = require('gulp-sass')(require('sass')); //const sass = requiere("sass") this work only with sass // export 
 const plumber = require('gulp-plumber');
+
+//images
+const cache = require('gulp-cache');
+const imagemin = require( 'gulp-imagemin' );
+const webp = require('gulp-webp');
 
 function css(done) {
     src("src/scss/**/*.scss") //src("src/scss/app.scss") //identify this file SASS
@@ -11,6 +17,26 @@ function css(done) {
     done(); //callback that notifies gulp when we reach the end
 }
 
+function imagenes( done ) {
+    const options = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache(imagemin(options)) )
+        .pipe( dest('build/img') )
+    done();
+}
+
+function versionWebp ( done ) {
+    const options = {
+        quality: 50
+    };
+    src( 'src/img/**/*.{png,jpg}' )
+        .pipe( webp(options) )
+        .pipe( dest( 'build/img' ))
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css)
 
@@ -18,6 +44,8 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
 
 // don't forget to install gulp-sass check that ein gulp.com
